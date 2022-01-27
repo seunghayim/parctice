36205ef84214aeadd1dad58261b3a9d08056374d
seunghayim <97645665+seunghayim@users.noreply.github.com>
2022-01-27 17:59:43 +0900

    Update app.py

--- 0127/flaskr/app.py
+++ 0127/flaskr/app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from pakage.vaccadd import VaccPublicData
 
 # 인스턴스 선언 및 app의 위치를 알리기 위해 __name__
 app = Falsk(__name__)
@@ -8,4 +9,6 @@ app.config['JSON_AS_ASCII'] = False
 # 데코레이션
 @app.route('/')
 def flaskData():
-    pass
+    # 인스턴스 
+    data1 = VaccPublicData(1,5,"중랑구")
+    return data1.vaccadd
